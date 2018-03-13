31beaee37c78b333b1549395cf2c0585a31c91e0
Sage <sage.b.shaw@gmail.com>
2018-03-13 17:59:39 -0600

    NM fixed

--- matlab/ch09_p08.m
+++ matlab/ch09_p08.m
@@ -1,10 +1,10 @@
 N = 2^7-1;
 n = N^2;
 h = 1/(N+1);
-a = .5;
-itermax = 10^5;
+a = .1;
+itermax = 10^6;
 %itermax = 0;
-tol = 10^-3;
+tol = 10^-6;
 
 
 K = diag(repelem(4,N)) + diag(repelem(-1,N-1),1) + diag(repelem(-1,N-1),-1);
@@ -16,21 +16,21 @@ u = zeros(n,1);
 for i=1:N
    for j=1:N
       x = h*j;
-      y = h*i;
-      u((j-1)*N+i) = a*x*(1-x)*(1-y);
+      y =                                                                                                                                                                                                                                                                                                                                                                                                                                    h*i;
+      u((j-1)*N+i) = a*x*(1-x)*(1-y);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
    end
 end
 
 Jac = A - sparse(diag(exp(u)*h^2));
-u_new = Jac\(h^2*exp(u) - A*u);
+u_new = u + (Jac\(h^2*exp(u) - A*u));
 for i=1:itermax
     if norm(u_new-u, inf)<tol
-       break 
+       break                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
        disp(i)
     end
     u = u_new;
-    Jac = A - diag(exp(u)*h^2);
-    u_new = Jac\(h^2*exp(u) - A*u);
+    Jac = A - sparse(diag(exp(u)*h^2));
+    u_new = u + (Jac\(h^2*exp(u) - A*u));
 end
 
 disp(i)
